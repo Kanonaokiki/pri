d355aa00fe41562cd7ba0b361a039cfda37cebc2
Kanonaokiki <117688935+Kanonaokiki@users.noreply.github.com>
2023-03-05 13:20:20 +0900

    課題

---
+++ 1-9/test1.java
@@ -0,0 +1,14 @@
+
+public class test1{
+    public static void main(String[] args) {
+
+//      1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+        int i=5;
+        int y=9;
+        int z= i+y;
+        System.out.println("i");
+//      2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+
+//      3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。
+    }
+}
---
+++ 1−9/src/test1.java
@@ -0,0 +1,23 @@
+
+public class test1{
+	public static void main(String[] args) {
+
+		//      1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+		int x=5;
+		int y=9;
+		int z= x+y;
+
+		System.out.println(z);
+
+		//      2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+		double d=1111111111*2;
+
+		System.out.println(d);
+
+		//      3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。
+		int num1 = 1;
+		int num2 = 10;
+
+		System.out.println(num1 == num2);
+	}
+}
